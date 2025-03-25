06865dca9569af898bfb9b873ec1acff617af9c4
rushikeshkamurthi <k.hrushikesh007@gmail.com>
2025-03-25 19:04:21 +0530

    app .js file update

--- App.js
+++ App.js
@@ -1,16 +1,24 @@
-import React from 'react';
-import { PaperProvider } from 'react-native-paper';
-import { AuthProvider } from './src/context/AuthContext';
-import { ThemeProvider } from './src/context/ThemeContext';
-import Routes from './src/routes/Routes';
+import React, {useContext} from 'react';
+import {PaperProvider} from 'react-native-paper';
+import {ThemeProvider, ThemeContext} from './src/context/ThemeContext';
+import {AuthProvider} from './src/context/AuthContext';
+import AppNavigator from './src/navigation/AppNavigator';
 import lightTheme from './src/theme/lightTheme';
+import darkTheme from './src/theme/darkTheme';
+
+const MainApp = () => {
+  const theme = useContext(ThemeContext); // `theme` might be undefined
+  return (
+    <PaperProvider theme={theme?.isDarkTheme ? darkTheme : lightTheme}>
+      <AppNavigator />
+    </PaperProvider>
+  );
+};
 
 const App = () => (
   <ThemeProvider>
     <AuthProvider>
-      <PaperProvider theme={lightTheme}>
-        <Routes /> 
-      </PaperProvider>
+      <MainApp />
     </AuthProvider>
   </ThemeProvider>
 );
